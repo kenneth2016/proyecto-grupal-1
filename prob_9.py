ddb7cbe6469addfa3919b8fdf9e9aad5a7fa5bd0
kenneth2016 <kennethl1801@gmail.com>
2016-06-01 08:48:18 -0600

    solucion problema 9

--- prob_9.py
+++ prob_9.py
@@ -1,1 +1,9 @@
+def prob_9 (n1, n2, n3, l):
+	l=[n1, n2, n3]
+	l.sort()
+	if l[2] == ((l[0]**2 + l[1]**2)**0.5):
+		return "es una tripleta pitagórica"
+	else:
+		return "no es una tripleta pitagórica"
+
 
