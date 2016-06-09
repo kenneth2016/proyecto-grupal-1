d57656cb40ac7d4be4f8dce631650a65de5b46ef
kenneth2016 <kennethl1801@gmail.com>
2016-06-09 08:43:12 -0600

    PROBLEMA 10

---
+++ prob_10.py
@@ -0,0 +1,9 @@
+def prob_10 (p1, p2, p3):
+	p1 = ()
+	p2 = ()
+	p3 = ()
+	n1x=int(input("ingrese el punto x: "))
+	p1.append(n1x)
+	n1y=int(input("ingrese el punto y: "))
+	p1.append(n1y)
+	return (p1)
